f79e43769e427c1d3f323d98280da33c53e44a5a
SAN <florescencebit@gmail.com>
2025-07-06 12:30:40 +0530

    lenght of string

--- Array & String/array_string.cpp
+++ Array & String/array_string.cpp
@@ -1,7 +1,11 @@
+// length of the string
 #include <iostream>
 using namespace std;
 
 int main() {
-    
+    char s [] = "hello\0world"; // string terminate when it got first null char (\0)
+    int cnt = 0;
+    for (char c:s) if(c != '\0') cnt++; else break;
+    cout << "length : " << cnt;
     return 0;
 }
